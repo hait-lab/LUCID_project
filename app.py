d1bc360ba0bbcb1abd031ad56a3e5fd79d0a2cc5
Yuta Shoji <p108053y@mail.kyutech.jp>
2019-11-30 17:35:10 +0900

    画像保存機能

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from importlib import import_module
 import os
-from flask import Flask, render_template, Response
-
+from flask import Flask, render_template, Response, redirect
+import cv2
+import numpy as np
 # import camera driver
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
@@ -32,6 +33,20 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/send',methods=['GET', 'POST'])
+def send():
+    def gen2(camera):
+        return camera.get_frame()
+
+    frame = gen2(Camera())
+    nparr = np.frombuffer(frame, np.uint8)
+    img = cv2.imdecode(nparr,cv2.IMREAD_COLOR)
+
+    cv2.imwrite("test.jpg",img)
+    
+    return redirect('/')
 
 
 if __name__ == '__main__':
