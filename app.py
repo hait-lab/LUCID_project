9b0387972cf2e48c13a642598c272a26d4e7ce1d
Yuta Shoji <p108053y@mail.kyutech.jp>
2019-12-01 16:15:27 +0900

    STANDADテンプレートマッチング追加

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from importlib import import_module
 import os
 from random import randint
 from datetime import datetime
-from flask import Flask, render_template, Response, redirect,send_from_directory
+from flask import Flask, render_template, Response, redirect,send_from_directory,request
 import cv2
 import numpy as np
 
@@ -50,6 +50,38 @@ def send():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(SAVE_DIR, filename)
+
+def tempmath(img_path,temp_path,threshold = 0.5):
+    img = cv2.imread(img_path,0)
+    temp = cv2.imread(temp_path,0)
+    #マッチングテンプレートを実行
+    result = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF_NORMED)
+    #検出結果から検出領域の位置を取得
+    loc = np.where(result >= threshold)
+    #検出領域を四角で囲んで保存
+    result = cv2.imread(img_path)
+    w, h = temp.shape[::-1]
+    bias = 20
+    for top_left in zip(*loc[::-1]):
+        bottom_right = (top_left[0] + w-bias, top_left[1] + h-bias)
+        cv2.rectangle(result,(top_left[0]+bias,top_left[1]+bias), bottom_right, (0, 0, 255), 5)
+
+    cv2.imwrite(img_path[:-4]+temp_path[7:],result)
+    return img_path[:-4]+temp_path[7:]
+@app.route('/STANDARD',methods=['GET', 'POST'])
+def standard():
+    if request.method == 'POST':
+        res = request.form['get_value']  
+        standard_path = tempmath(res,'static/standard.png')
+
+    return render_template('index.html',original_url=res,standard_url=standard_path)
+
+@app.route('/LUCID',methods=['GET', 'POST'])
+def lucid():
+    if request.method == 'POST':
+        res = request.form['get_value']  
+
+    return redirect('/send')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
